de6ebdde7ef4bbe37be0d42bd800e62fce414140
haseeb318 <149112532+haseeb318@users.noreply.github.com>
2026-06-17 23:36:03 +0500

    css file  added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,11 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="min-h-screen">
+      {/* left side */}
+      <div className="hidden lg:flex lg:w-1/2 bg-app-green relative items-center justify-center"></div>
+      {/* right side */}
+    </div>
+  );
 }
 
 export default Login;
