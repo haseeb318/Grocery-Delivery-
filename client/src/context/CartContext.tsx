5becce7ea607060c90ddb9a91592b9a8311f8f74
haseeb318 <149112532+haseeb318@users.noreply.github.com>
2026-06-26 02:23:15 +0500

    Cart Sidebar submit

--- client/src/context/CartContext.tsx
+++ client/src/context/CartContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, type ReactNode, useContext } from "react";
+import {
+  createContext,
+  type ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import type { CartItem, Product } from "../types";
 interface CartContextType {
   items: CartItem[];
@@ -15,6 +21,47 @@ interface CartContextType {
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export function CartProvider({ children }: { children: ReactNode }) {
+  const [items, setItems] = useState<CartItem[]>(() => {
+    const saved = localStorage.getItem("app_cart");
+    return saved ? JSON.parse(saved) : [];
+  });
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  useEffect(() => {
+    localStorage.setItem("app_cart", JSON.stringify(items));
+  }, [items]);
+  const addToCart = (product: Product, quantity = 1) => {
+    setItems((prev) => {
+      const exiting = prev.find((item) => item.product._id === product._id);
+      if (exiting) {
+        return prev.map((item) =>
+          item.product._id === product._id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item,
+        );
+      }
+      return [...prev, { product, quantity }];
+    });
+    setIsCartOpen(true);
+  };
+  const removeFromCart = (productId: string) => {
+    setItems((prev) => prev.filter((item) => item.product._id !== productId));
+  };
+  const updateQuantity = (productId: string, quantity: number) => {
+    if (quantity <= 0) {
+      removeFromCart(productId);
+      return;
+    }
+    setItems((prev) =>
+      prev.map((item) =>
+        item.product._id === productId ? { ...item, quantity } : item,
+      ),
+    );
+  };
+  const clearCart = () => {
+    setItems([]);
+    setIsCartOpen(false);
+  };
+
   return <CartContext.Provider value={{}}>{children}</CartContext.Provider>;
 }
 
