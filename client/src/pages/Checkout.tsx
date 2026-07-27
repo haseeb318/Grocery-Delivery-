750938c57793269af77e2bd75d2eb4aea76e8d97
haseeb318 <149112532+haseeb318@users.noreply.github.com>
2026-07-27 23:53:22 +0500

    check out page is added

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,5 +1,204 @@
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { useState } from "react";
+import { dummyAddressData } from "../assets/data";
+import type { Address } from "../types";
+import CheckoutAddress from "../components/Checkout/CheckoutAddress";
+import {
+  ArrowLeft,
+  CheckIcon,
+  ChevronRightIcon,
+  CreditCardIcon,
+  MapPinIcon,
+} from "lucide-react";
+import CheckoutPayment from "../components/Checkout/CheckoutPayment";
+import CheckoutReview from "../components/Checkout/CheckoutReview";
+
 function Checkout() {
-  return <div>Checkout</div>;
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
+  const { items, cartTotal } = useCart();
+  const { user } = { user: { Addresses: dummyAddressData } };
+
+  const [step, setStep] = useState("address");
+  const [loading, setLoading] = useState(false);
+
+  const [address, setAddress] = useState<Address>({
+    _id: "",
+    label: "Home",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    isDefault: false,
+    lat: 0,
+    lng: 0,
+  });
+
+  const [paymentMethod, setPaymentMethod] = useState("card");
+  const deliveryFee = cartTotal > 20 ? 0 : 1.99;
+  const tax = cartTotal * 0.08;
+  const total = cartTotal + deliveryFee + tax;
+
+  const steps: { key: string; label: string; icon: typeof MapPinIcon }[] = [
+    { key: "address", label: "Address", icon: MapPinIcon },
+    { key: "payment", label: "Payment", icon: CreditCardIcon },
+    { key: "review", label: "Review", icon: CheckIcon },
+  ];
+
+  const handlePlaceHolder = async () => {
+    setLoading(true);
+    navigate("/order");
+  };
+  //populate address from user's default address
+  useState(() => {
+    if (user?.Addresses?.length) {
+      const defaultAddr =
+        user.Addresses.find((a) => a.isDefault) || user.Addresses[0];
+      setAddress({
+        _id: defaultAddr._id,
+        label: defaultAddr.label,
+        address: defaultAddr.address,
+        city: defaultAddr.city,
+        state: defaultAddr.state,
+        zip: defaultAddr.zip,
+        isDefault: defaultAddr.isDefault,
+        lat: defaultAddr.lat,
+        lng: defaultAddr.lng,
+      });
+    }
+  });
+
+  if (items.length === 0) {
+    return (
+      <div className="min-h-screen bg-app-cream flex-center">
+        <div className="text-center">
+          <h2 className="text-lg font-semibold text-app-green mb-2">
+            Your cart is empty
+          </h2>
+          <p className="text-sm font-semibold text-app-text-light mb-4">
+            Add some product to checkout
+          </p>
+          <button
+            onClick={() => navigate("/products")}
+            className="px-5 py-2.5
+          bg-app-green text-white text-sm font-medium rounded-xl 
+          hover:bg-app-green-light transition-colors"
+          >
+            Broswser Product
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="min-h-screen bg-app-cream">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {/* Back button */}
+        <button
+          onClick={() => navigate(-1)}
+          className="flex items-center gap-2 text-sm text-app-text-light 
+        hover:text-app-green mb-6 transition-colors"
+        >
+          <ArrowLeft className="size-4" />
+          Back
+        </button>
+        <h1 className="text-2xl font-semibold text-app-green mb-8">Checkout</h1>
+        {/* steps  */}
+        <div className="flex items-center gap-2 mb-8">
+          {steps.map((s, i) => (
+            <div key={s.key} className="flex items-center gap-2 ">
+              <button
+                onClick={() => setStep(s.key)}
+                className={`flex item-center gap-2 px-4 py-2 rounded-xl text-sm font-medium
+                transition-colors ${step === s.key ? "bg-app-green text-white" : "bg-white text-app-text-light"}`}
+              >
+                <s.icon className="size-4" />
+                {s.label}
+                {i < step.length - 1 && (
+                  <ChevronRightIcon className="size-4 text-app-text-light" />
+                )}
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="grid md:grid-cols-3 gap-6">
+          {/* Main form */}
+          <div className="md:col-span-2">
+            {step === "address" && (
+              <CheckoutAddress
+                address={address}
+                setAddress={setAddress}
+                setStep={setStep}
+                user={user}
+              />
+            )}
+            {step === "payment" && (
+              <CheckoutPayment
+                paymentMethod={paymentMethod}
+                setPaymentMethod={setPaymentMethod}
+                setStep={setStep}
+              />
+            )}
+            {step === "address" && (
+              <CheckoutReview
+                address={address}
+                items={items}
+                handlePlaceOrder={handlePlaceHolder}
+                loading={loading}
+                total={total}
+              />
+            )}
+          </div>
+
+          {/* Order summary sidebar  */}
+          <div className="bg-white p-5 rounded-2xl h-fit stricky top-24">
+            <h3 className="text-sm font-semibold text-app-green mb-4">
+              Order Summary
+            </h3>
+            <div className="space-y-2 text-sm">
+              <div className="flex justify-between">
+                <span className="text-app-text-light">
+                  Subtotal {items.length} items
+                </span>
+                <span>
+                  {currency} {cartTotal.toFixed(2)}
+                </span>
+              </div>
+
+              <div className="flex justify-between">
+                <span className="text-app-text-light">Delivery</span>
+                <span>
+                  {deliveryFee === 0 ? (
+                    <span className="text-app-success">Free</span>
+                  ) : (
+                    `${currency}${deliveryFee.toFixed(2)}`
+                  )}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-app-text-light">Tax</span>
+                <span>
+                  {currency}
+                  {tax.toFixed(2)}
+                </span>
+              </div>
+              <div
+                className="flex justify-between pt-3 border-t
+               border-app-border text-base font-semibold"
+              >
+                <span>Total</span>
+                <span className="text-app-green">
+                  {currency}
+                  {total.toFixed(2)}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
